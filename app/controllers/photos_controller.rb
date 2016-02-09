5b29f036dc8b106c98d0202871aacd13f68d6fd5
Alex Pounds <alex+git@alexpounds.com>
2016-02-09 16:55:08 -0500

    Cache data for 1 minute.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,12 @@
 class PhotosController < ApplicationController
+  # Display a page showing the most popular images right now.
   def popular
-    @photos = photo_service.most_popular
+    @photos = Rails.cache.fetch("most_popular", expires_in: 1.minute) { photo_service.most_popular }
   end
 
   protected
 
+  # Putting this in its own method will make it easy for us to provide stub data when we want to write tests.
   def photo_service
     @service ||= FiveHundredPxService.new
   end
