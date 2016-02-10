da234f7040630322b485b9c35936ccca82d3d10a
Alex Pounds <alex+git@alexpounds.com>
2016-02-10 17:15:32 -0500

    Save the oAuth details into the session too.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   def create
     session[:user] = auth_hash.info.nickname
+    session[:oauth_token] = auth_hash.credentials.token
+    session[:oauth_secret] = auth_hash.credentials.secret
     redirect_to root_path, notice: "Signed in as #{auth_hash.info.nickname}."
   end
 
