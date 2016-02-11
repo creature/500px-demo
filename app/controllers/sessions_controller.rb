f43cc3378bae458c2683eef93eb021435ee177ec
Alex Pounds <alex+git@alexpounds.com>
2016-02-11 18:13:19 -0500

    Save likes on our end, and tweak our session handling a little.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,21 @@
 class SessionsController < ApplicationController
+  # Log a user in by saving the relevant info to the session.
   def create
+    session[:uid] = auth_hash.uid
     session[:user] = auth_hash.info.nickname
     session[:oauth_token] = auth_hash.credentials.token
     session[:oauth_secret] = auth_hash.credentials.secret
     redirect_to root_path, notice: "Signed in as #{auth_hash.info.nickname}."
   end
 
+  # Log a user out by removing data from the session.
   def destroy
-    session[:user] = nil
+    [:uid, :user, :oauth_token, :oauth_secret].map { |sym| session[sym] = nil }
     redirect_to root_path, notice: "You've been signed out."
   end
 
   def failure
-    logger.debug(request.env['omniauth.auth'])
+    logger.debug(auth_hash.inspect)
     redirect_to root_path, alert: "Sorry, we couldn't log you in. :("
   end
 
